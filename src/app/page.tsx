d2110a7b0801a275e2dc28c6fbf8612f75103857
Bao Kien <baokien2000@gmail.com>
2024-01-15 14:21:25 +0700

    change color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="text-black">
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <Image src="/images/logo.png" alt="Logo" width="200" height="200" />
-        <h1 className="text-6xl font-bold">Hello World</h1>
-        <h1 className="text-6xl font-bold">Hello World</h1>
-        <h1 className="text-6xl font-bold">Hello World</h1>
-        <h1 className="text-6xl font-bold">Hello World</h1>
-        <h1 className="text-6xl font-bold">Hello World</h1>
+      <div className="text-black">
+          <div className="flex flex-col items-center justify-center min-h-screen py-2">
+              <Image src="/images/logo.png" alt="Logo" width="200" height="200" />
+              <h1 className="text-6xl font-bold bg-[red]">Hello World</h1>
+              <h1 className="text-6xl font-bold">Hello World</h1>
+              <h1 className="text-6xl font-bold">Hello World</h1>
+              <h1 className="text-6xl font-bold">Hello World</h1>
+              <h1 className="text-6xl font-bold">Hello World</h1>
+          </div>
       </div>
-    </div>
   );
 }
